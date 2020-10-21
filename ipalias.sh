98549bfe2ef9c9d660c8393ef5f03bb72b318b3a
Charlie Root <root@aqua4.blr.asicdesigners.com>
2020-10-21 10:25:13 +0530

    Modified w_g_e_t_download_loop_with_mtu_change.sh

--- ipalias.sh
+++ ipalias.sh
@@ -1,5 +1,5 @@
-for ((i=1; i<=50; i++))
+for ((i=1; i<=100; i++))
 do
-	ifconfig cc0 103.$i.$i.77/24 alias
-	ifconfig cc1 104.$i.$i.77/24 alias
+	ifconfig cc0 103.$i.$i.85/24 alias
+	ifconfig cc1 104.$i.$i.85/24 alias
 done
--- w_g_e_t_download_loop_with_mtu_change.sh
+++ w_g_e_t_download_loop_with_mtu_change.sh
@@ -1,3 +1,6 @@
+ifconfig cc0 mtu 9600 up 
+ifconfig cc1 mtu 9600 up
+ssh 10.193.80.89 "ifconfig cc0 mtu 1500 up && ifconfig cc1 mtu 1500 up"
 rm -rf iter_test_file
 rm -rf *.log
 run_wget() {
@@ -7,12 +10,12 @@ run_wget() {
         done
 
 }
-mtu=(1280 1488 1500 2002 2048 4096 4352 8192 9000 9600)
+mtu=(88 256 512 576 808 1024 1280 1488 1500 2002 2048 4096 4352 8192 9000 9600)
 i=0
 while [ $i -lt 5001 ]
 do
 Num_Client=`ps -ef | grep wget | wc -l`
-if [ $Num_Client -eq 1 ]
+if [ $Num_Client -eq 0 ]
 then
 	i=`expr $i + 1`
 	echo "Iteration: $i" >> iter_test_file
@@ -20,21 +23,23 @@ then
 	if [ $((i % 10)) -eq 0 ]
 	then
         		pos=`expr $(((i/10)%${#mtu[@]})) - 1`
-        		echo "MTU on cool1 == ${mtu[$pos]}" >> iter_test_file
-			ssh 10.193.80.77 "ifconfig cc0 mtu ${mtu[$pos]} && ifconfig cc1 mtu ${mtu[$pos]}"
+        		echo "MTU on aqua6 == ${mtu[$pos]}" >> iter_test_file
+			ssh 10.193.80.89 "ifconfig cc0 mtu ${mtu[$pos]} && ifconfig cc1 mtu ${mtu[$pos]}"
     		
 	else
 		echo "Value of i == $i Iteration is not divisible by 10" >> iter_test_file
 	fi 
-	for ((j=1; j<=50; j++))
+	for ((j=1; j<=75; j++))
         do
-              	run_wget 103.$j.$j.77
-               	run_wget 104.$j.$j.77
+              	#run_wget 103.$j.$j.89
+               	#run_wget 104.$j.$j.89
+		run_wget [3000:$i::89]
+		run_wget [4000:$i::89]
         done
 else 
 	sleep 20
 fi 
-echo 1 > /proc/sys/vm/drop_caches
-echo 2 > /proc/sys/vm/drop_caches
-echo 3 > /proc/sys/vm/drop_caches
+#echo 1 > /proc/sys/vm/drop_caches
+#echo 2 > /proc/sys/vm/drop_caches
+#echo 3 > /proc/sys/vm/drop_caches
 done
